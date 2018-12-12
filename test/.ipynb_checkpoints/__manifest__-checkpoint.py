bcfadae89dd3b5cfd03b04556fd92dc52ea0e954
Ud0209 <45680210+Ud0209@users.noreply.github.com>
2018-12-12 15:30:08 +0000

    UD

---
+++ test/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': '练习测试',
+    'version': '12.0.1.0',
+    'summary': 'UD_Test',
+    'author': "www.mypscloud.com",
+    'website': 'https://www.mypscloud.com/',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': ['views/test_views.xml',]
+    'demo': [],
+    'qweb': [],
+    'js': [],
+    'css': [],
+    'auto_install': False,
+    'application': True,
+}
